f5a7d9834c8c642d92f2d8c553295152a0ca6fbf
earoflot <quaremanish@gmail.com>
2024-12-19 16:29:16 +0530

    img

--- airlines/src/App.jsx
+++ airlines/src/App.jsx
@@ -2,12 +2,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import NavigationDrawer from "./Components/NavigationDrawer/NavigationDrawer";
 import Footer from "./Components/Footer/Footer";
 import "./App.css";
+import HomeImages from "./Components/Main/Home_imges/HomeImages";
 
 function App() {
   return (
     <>
-      <NavigationDrawer></NavigationDrawer>
-      <Footer></Footer>
+      <div className="container-fluid">
+        <div className="container">
+          <NavigationDrawer></NavigationDrawer>
+          <HomeImages></HomeImages>
+
+          <Footer></Footer>
+        </div>
+      </div>
     </>
   );
 }
---
+++ airlines/src/Components/Main/Home_imges/HomeImages.jsx
@@ -0,0 +1,128 @@
+function Home() {
+  return (
+    <>
+      {/* This is a Gound Handling */}
+      <div class="card mb-3">
+        <img
+          src="./images/GroundHandling.jpeg"
+          class="card-img-top"
+          alt="..."
+        />
+        <div class="card-body">
+          <h5 class="Gound Handling">Ground Handling</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <p class="card-text">
+            <small class="text-body-secondary">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+      {/* This is a Retails Shop */}
+
+      <div class="card">
+        <img src="./images/Airindia.jpg" class="card-img-bottom" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Airport Retails Shops</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <p class="card-text">
+            <small class="text-body-secondary">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+      {/* Maintenance and Engineering */}
+      <div class="card">
+        <img src="./images/Airindia.jpg" class="card-img-bottom" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Maintenance & Engineering</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <p class="card-text">
+            <small class="text-body-secondary">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+      {/* Cabin Crew */}
+      <div class="card">
+        <img src="./images/Cabin_crew.jpg" class="card-img-bottom" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Cabin Crew</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <p class="card-text">
+            <small class="text-body-secondary">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+      {/* Assist Service */}
+      <div class="card">
+        <img src="./images/Airindia.jpg" class="card-img-bottom" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Assist Services</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <p class="card-text">
+            <small class="text-body-secondary">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+      <div class="card">
+        <img src="./images/Airindia.jpg" class="card-img-bottom" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Airport Security Officer</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <p class="card-text">
+            <small class="text-body-secondary">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+      <div class="card">
+        <img src="./images/Airindia.jpg" class="card-img-bottom" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Airlines Food Service Worker</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <p class="card-text">
+            <small class="text-body-secondary">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
+
+// function Home() {
+//   return (
+//     <>
+//       <div>
+//         {/* <img src="./images/inda_img.png" alt="" /> */}
+//         <img src="./images/Airindia.jpg" alt="" />
+//         <img src="./images/Arabia.jpg" alt="" />
+//         <img src="./images/Airashia.jpg" alt="" />
+
+//         <img src="./images/Indigo.jpg" alt="" />
+
+//         <img src="./images/Qatar.png" alt="" />
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Home;
