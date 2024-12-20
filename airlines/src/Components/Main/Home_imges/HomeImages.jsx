83ec0c69a3b67a3f282c8048d63a40b311ec32da
earoflot <quaremanish@gmail.com>
2024-12-20 14:07:27 +0530

    images set

--- airlines/src/Components/Main/Home_imges/HomeImages.jsx
+++ airlines/src/Components/Main/Home_imges/HomeImages.jsx
@@ -4,15 +4,16 @@ function Home() {
       {/* This is a Gound Handling */}
       <div class="card mb-3">
         <img
-          src="./images/GroundHandling.jpeg"
+          src="./images/Ground_Handling.jpeg"
           class="card-img-top"
           alt="..."
         />
         <div class="card-body">
           <h5 class="Gound Handling">Ground Handling</h5>
           <p class="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
+            Ground handling is a vital part of the aviation industry that
+            involves the services required to operate an aircraft while it's on
+            the ground at an airport
           </p>
           <p class="card-text">
             <small class="text-body-secondary">Last updated 3 mins ago</small>
@@ -30,8 +31,9 @@ function Home() {
         <div class="card-body">
           <h5 class="card-title">Airport Retails Shops</h5>
           <p class="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
+            As a retail team member, you'll play a key role in engaging
+            customers by promoting, advising on, and selling a wide range of
+            exclusive tax and duty-free products.
           </p>
           <p class="card-text">
             <small class="text-body-secondary">Last updated 3 mins ago</small>
@@ -40,12 +42,18 @@ function Home() {
       </div>
       {/* Maintenance and Engineering */}
       <div class="card">
-        <img src="./images/Vistara.webp" class="card-img-bottom" alt="..." />
+        <img
+          src="./images/Mantanance_Engeenering.jpeg"
+          class="card-img-bottom"
+          alt="..."
+        />
         <div class="card-body">
           <h5 class="card-title">Maintenance & Engineering</h5>
           <p class="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
+            A maintenance engineer is responsible for undertaking continuous
+            routine maintenance on plants and machinery to ensure they run
+            smoothly and reliably. They work with other professionals to improve
+            efficiency and safety standards as well as organise repairs.
           </p>
           <p class="card-text">
             <small class="text-body-secondary">Last updated 3 mins ago</small>
@@ -58,8 +66,11 @@ function Home() {
         <div class="card-body">
           <h5 class="card-title">Cabin Crew</h5>
           <p class="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
+            Cabin crew also known as Flight Attendants provide in-flight
+            services to ensure that airline passengers have a comfortable and
+            smooth flying experience. Their duties include answering passengers’
+            questions, enforcing safety measures before, during and after
+            takeoff and serving food and beverages during flights.
           </p>
           <p class="card-text">
             <small class="text-body-secondary">Last updated 3 mins ago</small>
@@ -68,38 +79,59 @@ function Home() {
       </div>
       {/* Assist Service */}
       <div class="card">
-        <img src="./images/Airlines.webp" class="card-img-bottom" alt="..." />
+        <img
+          src="./images/Assist_Services.jpeg"
+          class="card-img-bottom"
+          alt="..."
+        />
         <div class="card-body">
           <h5 class="card-title">Assist Services</h5>
           <p class="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
+            A assist services or Customer Service Representative provides a
+            company’s customers with the service they require. Their duties
+            include receiving inbound calls, responding to complaints and
+            selling a company’s products or services.
           </p>
           <p class="card-text">
             <small class="text-body-secondary">Last updated 3 mins ago</small>
           </p>
         </div>
       </div>
+      {/* Airport Security Officer */}
       <div class="card">
-        <img src="./images/Airindia.jpg" class="card-img-bottom" alt="..." />
+        <img
+          src="./images/Airport_Security.jpeg"
+          class="card-img-bottom"
+          alt="..."
+        />
         <div class="card-body">
           <h5 class="card-title">Airport Security Officer</h5>
           <p class="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
+            The duties of an airport security employee may vary depending on
+            their designation and specialisation. These professionals work in
+            teams, with government officials and law enforcement agents to
+            ensure the security and safety of passengers and personnel in an
+            airport.
           </p>
           <p class="card-text">
             <small class="text-body-secondary">Last updated 3 mins ago</small>
           </p>
         </div>
       </div>
+      {/* Airport Food Service Worker */}
       <div class="card">
-        <img src="./images/Spice_Jet.webp" class="card-img-bottom" alt="..." />
+        <img
+          src="./images/Food_Worker.jpeg"
+          class="card-img-bottom"
+          alt="..."
+        />
         <div class="card-body">
           <h5 class="card-title">Airlines Food Service Worker</h5>
           <p class="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
+            Airline food service workers typically work in the galley of an
+            aircraft, where they provide passengers with meals, and perform food
+            preparation, sanitation, and stocking duties. They may also serve
+            special meals for passengers with dietary needs.
           </p>
           <p class="card-text">
             <small class="text-body-secondary">Last updated 3 mins ago</small>
