cf1923faa2e83119e788309e334754d4048654d9
earoflot <quaremanish@gmail.com>
2024-12-20 21:26:55 +0530

    r2

--- airlines/src/main.jsx
+++ airlines/src/main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-
-import "bootstrap/dist/css/bootstrap.min.css";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
 import "./index.css";
 
-const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+const router = createBrowserRouter([{ path: "/service/", element: <App /> }]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
