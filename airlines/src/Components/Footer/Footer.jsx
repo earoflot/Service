9dae110d64f3812478e0aae3708f5421c449627a
earoflot <quaremanish@gmail.com>
2024-12-20 19:38:34 +0530

    FOOTER

--- airlines/src/Components/Footer/Footer.jsx
+++ airlines/src/Components/Footer/Footer.jsx
@@ -1,40 +1,84 @@
+import React from "react";
+
 function Footer() {
   return (
-    <>
-      <div class="container">
-        <footer class="py-3 my-4">
-          <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-            <li class="nav-item">
-              <a href="#" class="nav-link px-2 text-body-secondary">
-                Home
+    <div className="container">
+      <footer className="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+        <div className="col mb-3"></div>
+
+        <div className="col mb-3">
+          <h5>QUICK LINKS</h5>
+          <ul className="nav flex-column">
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                HOME
               </a>
             </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link px-2 text-body-secondary">
-                Features
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                NOTIFICATIONS
               </a>
             </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link px-2 text-body-secondary">
-                Pricing
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                APPLY
               </a>
             </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link px-2 text-body-secondary">
-                FAQs
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                JOB VACANCY
               </a>
             </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link px-2 text-body-secondary">
-                About
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                PLACEMENT
               </a>
             </li>
           </ul>
-          <p class="text-center text-body-secondary">© 2024 Company, Inc</p>
-        </footer>
-      </div>
-    </>
+        </div>
+
+        <div className="col mb-3">
+          <h5>OUR ADDRESS</h5>
+          <ul className="nav flex-column"></ul>
+        </div>
+
+        <div className="col mb-3">
+          <h5>CONTACT US</h5>
+          <ul className="nav flex-column"></ul>
+        </div>
+        <div className="col mb-3">
+          <h5>FOLLOW US</h5>
+          <ul className="nav flex-column"></ul>
+        </div>
+      </footer>
+    </div>
   );
 }
 
 export default Footer;
+
+// / <div className="col mb-3">
+{
+  /* <a */
+}
+// href="/"
+// className="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none"
+{
+  /* > */
+}
+{
+  /* <svg className="bi me-2" width="40" height="32"> */
+}
+{
+  /* <use xlinkHref="#bootstrap"></use> */
+}
+{
+  /* </svg> */
+}
+{
+  /* </a> */
+}
+{
+  /* <p className="text-body-secondary">© 2024</p> */
+}
+// </div>
--- airlines/src/Pages/Service/Services.jsx
+++ airlines/src/Pages/Service/Services.jsx
@@ -3,139 +3,157 @@ function Services() {
   return (
     <>
       {/* This is a Gound Handling */}
-      <div class="card mb-3">
+      <div className="card mb-3">
         <img
           src="./images/Ground_Handling.jpeg"
-          class="card-img-top"
+          className="card-img-top"
           alt="..."
         />
-        <div class="card-body">
-          <h5 class="Gound Handling">Ground Handling</h5>
-          <p class="card-text">
+        <div className="card-body">
+          <h5 className="Gound Handling">Ground Handling</h5>
+          <p className="card-text">
             Ground handling is a vital part of the aviation industry that
             involves the services required to operate an aircraft while it's on
             the ground at an airport.
           </p>
-          <p class="card-text">
-            <small class="text-body-secondary">Last updated 3 mins ago</small>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              Last updated 3 mins ago
+            </small>
           </p>
         </div>
       </div>
       {/* This is a Retails Shop */}
 
-      <div class="card">
+      <div className="card">
         <img
           src="./images/Retail_shop.avif"
-          class="card-img-bottom"
+          className="card-img-bottom"
           alt="..."
         />
-        <div class="card-body">
-          <h5 class="card-title">Airport Retails Shops</h5>
-          <p class="card-text">
+        <div className="card-body">
+          <h5 className="card-title">Airport Retails Shops</h5>
+          <p className="card-text">
             As a retail team member, you'll play a key role in engaging
             customers by promoting, advising on, and selling a wide range of
             exclusive tax and duty-free products.
           </p>
-          <p class="card-text">
-            <small class="text-body-secondary">Last updated 3 mins ago</small>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              Last updated 3 mins ago
+            </small>
           </p>
         </div>
       </div>
       {/* Maintenance and Engineering */}
-      <div class="card">
+      <div className="card">
         <img
           src="./images/Mantanance_Engeenering.jpeg"
-          class="card-img-bottom"
+          className="card-img-bottom"
           alt="..."
         />
-        <div class="card-body">
-          <h5 class="card-title">Maintenance & Engineering</h5>
-          <p class="card-text">
+        <div className="card-body">
+          <h5 className="card-title">Maintenance & Engineering</h5>
+          <p className="card-text">
             A maintenance engineer is responsible for undertaking continuous
             routine maintenance on plants and machinery to ensure they run
             smoothly and reliably. They work with other professionals to improve
             efficiency and safety standards as well as organise repairs.
           </p>
-          <p class="card-text">
-            <small class="text-body-secondary">Last updated 3 mins ago</small>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              Last updated 3 mins ago
+            </small>
           </p>
         </div>
       </div>
       {/* Cabin Crew */}
-      <div class="card">
-        <img src="./images/Cabin_crew.jpg" class="card-img-bottom" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Cabin Crew</h5>
-          <p class="card-text">
+      <div className="card">
+        <img
+          src="./images/Cabin_crew.jpg"
+          className="card-img-bottom"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Cabin Crew</h5>
+          <p className="card-text">
             Cabin crew also known as Flight Attendants provide in-flight
             services to ensure that airline passengers have a comfortable and
             smooth flying experience. Their duties include answering passengers’
             questions, enforcing safety measures before, during and after
             takeoff and serving food and beverages during flights.
           </p>
-          <p class="card-text">
-            <small class="text-body-secondary">Last updated 3 mins ago</small>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              Last updated 3 mins ago
+            </small>
           </p>
         </div>
       </div>
       {/* Assist Service */}
-      <div class="card">
+      <div className="card">
         <img
           src="./images/Assist_Services.jpeg"
-          class="card-img-bottom"
+          className="card-img-bottom"
           alt="..."
         />
-        <div class="card-body">
-          <h5 class="card-title">Assist Services</h5>
-          <p class="card-text">
+        <div className="card-body">
+          <h5 className="card-title">Assist Services</h5>
+          <p className="card-text">
             A assist services or Customer Service Representative provides a
             company’s customers with the service they require. Their duties
             include receiving inbound calls, responding to complaints and
             selling a company’s products or services.
           </p>
-          <p class="card-text">
-            <small class="text-body-secondary">Last updated 3 mins ago</small>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              Last updated 3 mins ago
+            </small>
           </p>
         </div>
       </div>
       {/* Airport Security Officer */}
-      <div class="card">
+      <div className="card">
         <img
           src="./images/Airport_Security.jpeg"
-          class="card-img-bottom"
+          className="card-img-bottom"
           alt="..."
         />
-        <div class="card-body">
-          <h5 class="card-title">Airport Security Officer</h5>
-          <p class="card-text">
+        <div className="card-body">
+          <h5 className="card-title">Airport Security Officer</h5>
+          <p className="card-text">
             The duties of an airport security employee may vary depending on
             their designation and specialisation. These professionals work in
             teams, with government officials and law enforcement agents to
             ensure the security and safety of passengers and personnel in an
             airport.
           </p>
-          <p class="card-text">
-            <small class="text-body-secondary">Last updated 3 mins ago</small>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              Last updated 3 mins ago
+            </small>
           </p>
         </div>
       </div>
       {/* Airport Food Service Worker */}
-      <div class="card">
+      <div className="card">
         <img
           src="./images/Food_Worker.jpeg"
-          class="card-img-bottom"
+          className="card-img-bottom"
           alt="..."
         />
-        <div class="card-body">
-          <h5 class="card-title">Airlines Food Service Worker</h5>
-          <p class="card-text">
+        <div className="card-body">
+          <h5 className="card-title">Airlines Food Service Worker</h5>
+          <p className="card-text">
             Airline food service workers typically work in the galley of an
             aircraft, where they provide passengers with meals, and perform food
             preparation, sanitation, and stocking duties. They may also serve
             special meals for passengers with dietary needs.
           </p>
-          <p class="card-text">
-            <small class="text-body-secondary">Last updated 3 mins ago</small>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              Last updated 3 mins ago
+            </small>
           </p>
         </div>
       </div>
@@ -144,22 +162,3 @@ function Services() {
 }
 
 export default Services;
-
-// function Home() {
-//   return (
-//     <>
-//       <div>
-//         {/* <img src="./images/inda_img.png" alt="" /> */}
-//         <img src="./images/Airindia.jpg" alt="" />
-//         <img src="./images/Arabia.jpg" alt="" />
-//         <img src="./images/Airashia.jpg" alt="" />
-
-//         <img src="./images/Indigo.jpg" alt="" />
-
-//         <img src="./images/Qatar.png" alt="" />
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Home;
