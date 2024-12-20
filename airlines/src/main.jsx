e272c037f6396a539206bb09e38163ed2aff5e64
earoflot <quaremanish@gmail.com>
2024-12-20 21:30:30 +0530

    r4

--- airlines/src/main.jsx
+++ airlines/src/main.jsx
@@ -5,7 +5,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
 import "./index.css";
 
-const router = createBrowserRouter([{ path: "/service/", element: <App /> }]);
+const router = createBrowserRouter([
+  { path: "https://earoflot.github.io/service/", element: <App /> },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
