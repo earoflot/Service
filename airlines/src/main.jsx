8fba575f03c622cf663e480774cbf4fd5e880710
earoflot <quaremanish@gmail.com>
2024-12-21 22:25:13 +0530

    m

--- airlines/src/main.jsx
+++ airlines/src/main.jsx
@@ -1,21 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import App from "./routes/App.jsx";
+// import "./index.css";
+// import Home from "./Pages/Home/Home.jsx";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/service/",
+//     element: <App />,
+//     children: [{ path: "/service/home/", element: <Home /> }],
+//   },
+// ]);
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
+// );
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./routes/App.jsx";
 import "./index.css";
-import Home from "./Pages/Home/Home.jsx";
+import App from "./routes/App.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/service/",
-    element: <App />,
-    children: [{ path: "/service/home/", element: <Home /> }],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
--- airlines/src/routes/App.jsx
+++ airlines/src/routes/App.jsx
@@ -1,11 +1,8 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Routes, Route } from "react-router-dom";
 import NavigationDrawer from "../Components/NavigationDrawer/NavigationDrawer";
 import Footer from "../Components/Footer/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Outlet } from "react-router-dom";
 import Vacancy from "../Pages/Service/Vacancy.jsx";
-import Home from "../Pages/Home/Home.jsx";
 
 function App() {
   return (
@@ -13,7 +10,8 @@ function App() {
       <div className="container-fluid">
         <div className="container"></div>
         <NavigationDrawer />
-        <Outlet />
+        <Vacancy />
+
         <Footer />
       </div>
     </>
