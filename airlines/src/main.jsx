aec1e9e03c9bfe8732df96d483b2a6b018d097bd
earoflot <quaremanish@gmail.com>
2024-12-21 13:25:12 +0530

    h

--- airlines/src/main.jsx
+++ airlines/src/main.jsx
@@ -2,27 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./routes/App.jsx";
 import "./index.css";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/service/",
-//     element: <App />,
-//     children: [
-//       {
-//         path: "/service/vacancy",
-//         element: <Vacancy />,
-//       },
-//     ],
-//   },
-// ]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
